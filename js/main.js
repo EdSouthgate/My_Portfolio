c90c23fcc0a46e737986aea9b94b6123eb7d2fbc
EdSouthgate <edsouthgate@me.com>
2016-07-05 11:34:11 +0100

    Added some JS for mobile nav.

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.mobile-nav-toggle').click(function(){
+    $('.mobile-nav').toggleClass('is-open');
+    $('.mobile-nav-toggle').toggleClass('is-open');
+
+  });
+
+});
---
+++ js/min/main-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".mobile-nav-toggle").click(function(){$(".mobile-nav").toggleClass("is-open"),$(".mobile-nav-toggle").toggleClass("is-open")})});
